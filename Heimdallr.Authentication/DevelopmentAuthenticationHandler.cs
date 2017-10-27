8facdb3b525ef185230f193022acf3ba7bc1b25b
Vladimir <siniavsky.vladimir@gmail.com>
2017-10-27 16:58:27 +0300

    added AuthenticationHandler

---
+++ Heimdallr.Authentication/DevelopmentAuthenticationHandler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Heimdallr.Authentication
+{
+    internal class DevelopmentAuthenticationHandler : AuthenticationHandler<DevelopmentAuthenticationOptions>
+    {
+        private const string _Scheme = "Basic";
+
+        public DevelopmentAuthenticationHandler(
+            IOptionsMonitor<DevelopmentAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+        
+        protected new DevelopmentAuthenticationEvents Events
+        {
+            get { return (DevelopmentAuthenticationEvents)base.Events; }
+            set { base.Events = value; }
+        }
+        
+        protected override Task<object> CreateEventsAsync() => Task.FromResult<object>(new DevelopmentAuthenticationEvents());
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            string authorizationHeader = Request.Headers["Authorization"];
+            if (string.IsNullOrEmpty(authorizationHeader))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            if (!authorizationHeader.StartsWith(_Scheme + ' ', StringComparison.OrdinalIgnoreCase))
+            {
+                return AuthenticateResult.NoResult();
+            }
+
+            string encodedCredentials = authorizationHeader.Substring(_Scheme.Length).Trim();
+
+            if (string.IsNullOrEmpty(encodedCredentials))
+            {
+                const string noCredentialsMessage = "No credentials";
+                Logger.LogInformation(noCredentialsMessage);
+                return AuthenticateResult.Fail(noCredentialsMessage);
+            }
+
+            try
+            {
+                string decodedCredentials = string.Empty;
+                try
+                {
+                    decodedCredentials = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCredentials));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Failed to decode credentials : {encodedCredentials}", ex);
+                }
+
+                var delimiterIndex = decodedCredentials.IndexOf(':');
+                if (delimiterIndex == -1)
+                {
+                    const string missingDelimiterMessage = "Invalid credentials, missing delimiter.";
+                    Logger.LogInformation(missingDelimiterMessage);
+                    return AuthenticateResult.Fail(missingDelimiterMessage);
+                }
+
+                var username = decodedCredentials.Substring(0, delimiterIndex);
+                var password = decodedCredentials.Substring(delimiterIndex + 1);
+
+                var validateCredentialsContext = new ValidateCredentialsContext(Context, Scheme, Options)
+                {
+                    Username = username,
+                    Password = password
+                };
+
+                await Options.Events.ValidateCredentials(validateCredentialsContext);
+
+                if (validateCredentialsContext.Result != null)
+                {
+                    var ticket = new AuthenticationTicket(validateCredentialsContext.Principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+
+                return AuthenticateResult.NoResult();
+            }
+            catch (Exception ex)
+            {
+                var authenticationFailedContext = new AuthenticationFailedContext(Context, Scheme, Options)
+                {
+                    Exception = ex
+                };
+
+                await Options.Events.AuthenticationFailed(authenticationFailedContext);
+
+                if (authenticationFailedContext.Result.Succeeded)
+                {
+                    return AuthenticateResult.Success(authenticationFailedContext.Result.Ticket);
+                }
+
+                if (authenticationFailedContext.Result.None)
+                {
+                    return AuthenticateResult.NoResult();
+                }
+
+                throw;
+            }
+        }
+
+        protected override Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            if (!Request.IsHttps && !Options.AllowInsecureProtocol)
+            {
+                const string insecureProtocolMessage = "Request is HTTP, Basic Authentication will not respond.";
+                Logger.LogInformation(insecureProtocolMessage);
+                Response.StatusCode = 500;
+                var encodedResponseText = Encoding.UTF8.GetBytes(insecureProtocolMessage);
+                Response.Body.Write(encodedResponseText, 0, encodedResponseText.Length);
+            }
+            else
+            {
+                Response.StatusCode = 401;
+
+                var headerValue = _Scheme + $" realm=\"{Options.Realm}\"";
+                Response.Headers.Append(HeaderNames.WWWAuthenticate, headerValue);
+            }
+
+            return Task.CompletedTask; ;
+        }
+    }
+}
