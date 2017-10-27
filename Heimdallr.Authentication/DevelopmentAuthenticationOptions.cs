ec982aa60d34c423866e99a2700d63e8db3f3e8c
Vladimir <siniavsky.vladimir@gmail.com>
2017-10-27 16:57:49 +0300

    added Options

---
+++ Heimdallr.Authentication/DevelopmentAuthenticationOptions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace Heimdallr.Authentication
+{
+    public class DevelopmentAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        private string _realm;
+        
+        public DevelopmentAuthenticationOptions()
+        {
+        }
+        
+        public string Realm
+        {
+            get
+            {
+                return _realm;
+            }
+
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !IsAscii(value))
+                {
+                    throw new ArgumentOutOfRangeException("Realm", "Realm must be US ASCII");
+                }
+
+                _realm = value;
+            }
+        }
+        
+        public bool AllowInsecureProtocol
+        {
+            get; set;
+        }
+        
+        public new DevelopmentAuthenticationEvents Events { get; set; } = new DevelopmentAuthenticationEvents();
+
+
+        private bool IsAscii(string input)
+        {
+            foreach (char c in input)
+            {
+                if (c < 32 || c >= 127)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
