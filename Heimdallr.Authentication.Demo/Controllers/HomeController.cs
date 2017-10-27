c18e7ad8bc542d15703243f19454780ea6e04ff8
Vladimir <siniavsky.vladimir@gmail.com>
2017-10-27 16:59:09 +0300

    Demo app

---
+++ Heimdallr.Authentication.Demo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Heimdallr.Authentication.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "DevelopmentAccess")]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+
+        [Route("Logout")]
+        [Authorize(Policy = "DevelopmentAccess")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(DevelopmentAuthenticationDefaults.AuthenticationScheme);
+
+            return Redirect("~/");
+        }
+    }
+}
---
+++ Heimdallr.Authentication.Demo/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Heimdallr.Authentication.Demo
+{
+    public class Startup
+    {
+        private readonly bool _dev;
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            _dev = env.IsDevelopment();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_dev)
+            {
+                services
+                    .AddAuthentication(DevelopmentAuthenticationDefaults.AuthenticationScheme)
+                    .AddDevelopment(new DevelopmentUser
+                    {
+                        Username = "svasorcery",
+                        Password = "gfhjkm123",
+                        Subject = "S-0-0-00-0000000000-0000000000-0000000000-0000",
+                        Roles = new string[] { "IT", "Developer" }
+                    });
+            }
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("DevelopmentAccess", policy => policy.RequireRole("Developer"));
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseAuthentication();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
